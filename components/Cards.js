b7b957c4ee15dd298ec76a923a317c08b3e00b0d
Amari Cross <amaricross77@gmail.com>
2022-11-27 16:18:28 -0500

    actually works now???

--- components/Cards.js
+++ components/Cards.js
@@ -1,6 +1,3 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
 export default function Cards() {
 
     return (
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
