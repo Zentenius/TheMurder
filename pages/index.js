9419147d8ec1465fe3fecd96db3a1c465c59607c
Amari Cross <amaricross77@gmail.com>
2022-11-27 16:15:22 -0500

    are you serious rn

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import Navbar from "../components/Navbar"
 import Hero from "../components/Hero"
 import Cards from "../components/Cards"
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+
 
 
 export default function Home() {
