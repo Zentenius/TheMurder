b133e52d6dff331ff9c3337f640530badfc565dc
Amari Cross <amaricross77@gmail.com>
2022-11-27 16:11:27 -0500

    idk

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Navbar from "../components/Navbar"
 import Hero from "../components/Hero"
 import Cards from "../components/Cards"
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 
 export default function Home() {
